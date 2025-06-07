82ad879a2198ca17339ee859539f2aeb82281de6
marcelo <marcelomartinsfarinassojr@gmail.com>
2025-06-07 17:44:32 -0300

    branch fix

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Card from "./components/Card.tsx"
+function App() {
+
+  return (
+    <>
+    <Card Item={"Faca"} price={1000} url="/b5c1ab23-8e82-4353-9811-1c005afd4dd9.jpg"/>
+    </>
+  )
+}
+
+export default App
